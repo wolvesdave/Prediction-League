1c2bdb62dd466897fea7b543967c6212d251085c
wolvesdave <wolvesdave@gmail.com>
2017-10-06 21:12:10 -0400

    begin v2

--- app.js
+++ app.js
@@ -1,82 +1,66 @@
-// Import required modules
 var express = require('express'),
     app = express(),
     engines = require('consolidate'),
     bodyParser = require('body-parser'),
     MongoClient = require('mongodb').MongoClient,
     assert = require('assert');
-    nunjucks = require('nunjucks');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
-var predictions = require('./routes/predictions');
-var fixtures = require('./routes/fixtures');
-
-var env = nunjucks.configure('/path/to/templates', {
-      tags: {
-        // or whatever other tag markers you'd like to use
-        blockStart: '<%',
-        blockEnd: '%>',
-        variableStart: '<$',
-        variableEnd: '$>',
-        commentStart: '<#',
-        commentEnd: '#>'
-      }
-    }); 
-
-// load mongoose package
-var mongoose = require('mongoose');
-
-// Use native Node promises
-mongoose.Promise = global.Promise;
-
-// connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/predictionleague')
-  .then(() =>  console.log('connection succesful'))
-  .catch((err) => console.error(err));
 
 app.engine('html', engines.nunjucks);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', index);
-app.use('/users', users);
-app.use('/fixtures', fixtures);
-app.use('/predictions', predictions);
+MongoClient.connect('mongodb://localhost:27017/predictionleague', function(err, db) {
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+    assert.equal(null, err);
+    console.log("Successfully connected to MongoDB.");
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.get('/', function(req, res, next) {
+        res.render('index',{});
     });
-  });
-}
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
+    app.get('/list_users', function(req, res, next) {
+      db.collection('users').find({}).toArray(function (err, docs) {
+            assert.equal(null, err);
+            res.render('list_users',{users : docs});
+        });
+    });
 
-var server = app.listen(3000, function() {
-    var port = server.address().port;
-    console.log('Express server listening on port %s', port);
+    app.get('/add_user', function(req, res, next) {
+        res.render('add_user',{});
+    });
+
+    app.post('/add_user', function(req, res, next) {
+        var name = req.body.name;
+        var email = req.body.email;
+
+        if (name == '' || email == '') {
+          console.log("Please enter all fields before hitting Submit");
+        }
+
+        else {
+          console.log("Storing user ", name, " with email/ID ", email);
+          db.collection('users').insertOne({
+              "_id": email,
+              "name" : name,
+              "jokersRemaining" : 38,
+              "totalScore" : 0,
+              "weeklyScore" : [0],
+              "monthlyScore" : [0]
+            }, function (err, r) {
+                assert.equal(null, err);
+                res.render("add_user_confirm", {message : r.insertedId});
+            });
+        }
+    });
+
+    app.use(function(req, res){
+        res.sendStatus(404);
+    });
+
+    var server = app.listen(3000, function() {
+        var port = server.address().port;
+        console.log('Express server listening on port %s.', port);
+    });
+
 });
