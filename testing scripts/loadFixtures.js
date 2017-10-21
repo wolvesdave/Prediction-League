41cfb686267f2fe84f4645293512448df22f6bf5
David Koppe <wolvesdave@gmail.com>
2017-10-20 21:13:55 -0400

    Partway through creating live XMLSoccer API request and parse to DB

--- testing scripts/loadFixtures.js
+++ testing scripts/loadFixtures.js
@@ -1,42 +1,77 @@
+var MongoClient = require('mongodb').MongoClient,
+    fs = require('fs'),
+    assert = require('assert'),
+    parseString = require('xml2js').parseString,
+    request = require('request'),
+    http = require('http');
 
-var mongoose = require('mongoose');
-var Fixture = require('../models/Fixture');
-var User = require('../models/User');
-var Prediction = require('../models/Prediction');
+http.post = require('http-post');
 
-var faker = require('faker');
-/* var dotenv = require('dotenv'); */
-const chalk = require('chalk'); 
-mongoose.Promise = global.Promise;
+    payload = {
+      'ApiKey':'QQQMPBBYBPJYCVJCBHFRQMFVOCOSLBPPCXVGWLRKRRKAEACUXC',
+      'seasonDateString':'1718',
+      'league':'Scottish Premier League'
+    };
 
-/* dotenv.load({ path: '.env.example' }); */
+MongoClient.connect('mongodb://localhost:27017/predictionleague', function(err, db) {
 
-mongoose.connect('mongodb://localhost/predictionleague');
-mongoose.connection.on('error', () => {
-  console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
-  process.exit();
+  http.post('http://www.xmlsoccer.com/FootballDataDemo.asmx/GetFixturesByLeagueAndSeason',
+  payload, function(res){
+
+      console.log(`STATUS: ${res.statusCode}`);
+      res.setEncoding('utf8');
+      console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+
+      res.on('data', function(chunk) {
+
+        console.log("Here's a chunk: ", chunk);
+
+        parseString(chunk, function (err, result) {
+
+            var inputmatches = result["XMLSOCCER.COM"].Match,
+                outputmatches = [];
+
+            console.log(inputmatches.length);
+
+             for (var i = 0, len = inputmatches.length; i < len; i++) {
+              /* console.log("Mapping match ID ", inputmatches[i].Id[0]); */
+              if (inputmatches[i].HomeGoals !== undefined) {
+                homegoals = parseInt(inputmatches[i].HomeGoals[0])
+              } else {
+                homegoals = ""
+              }
+              if (inputmatches[i].AwayGoals !== undefined) {
+                awaygoals = parseInt(inputmatches[i].AwayGoals[0])
+              } else {
+                awaygoals = ""
+              }
+              match = {
+                "_id" : inputmatches[i].Id[0],
+                "Round" : parseInt(inputmatches[i].Round[0]),
+                "Date" : inputmatches[i].Date[0],
+                "HomeTeam" : inputmatches[i].HomeTeam[0],
+                "HomeGoals" : homegoals,
+                "AwayTeam" : inputmatches[i].AwayTeam[0],
+                "AwayGoals" : awaygoals,
+                "Location" : inputmatches[i].Location[0]
+              }
+              outputmatches.push(match);
+            };
+
+            /* console.log(outputmatches); */
+
+            db.collection('fixtures').insertMany(outputmatches, function(err, r) {
+
+              assert.equal(null, err);
+              console.log('Fixtures inserted with result ', r);
+
+            });
+
+
+        });
+
+      });
+
+  });
+
 });
-
-var fixture = new Fixture(
-        { "_id" : 1,
-        "League" : "Scottish Premier League",
-        "HomeTeam_Id" : 10,
-        "HomeTeam" : "Rangers",
-        "AwayGoals" : 2,
-        "AwayTeam_Id" : 20,
-        "AwayTeam" : "Celtic",
-        "HomeGoals" : 1,
-        "Location" : "Ibrox Stadium",
-        "Date" : 10/8/2016,
-        "Round" : 1}
-      );    
-    
-fixture.save(function(err) {
-	if (err) {
-		console.log('error: ',err.message);
-	}
-});
-	
-function exit() {
-	mongoose.disconnect();
-}
